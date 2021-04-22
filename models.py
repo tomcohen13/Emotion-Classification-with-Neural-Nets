5cf3c03ec069aa188be0945561485ffab22f789b
Tom <55168908+tomcohen13@users.noreply.github.com>
2021-04-22 12:15:13 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,9 +9,6 @@ BATCH_SIZE    = 128
 class DenseNetwork(nn.Module):
     def __init__(self, embeddings):
         super(DenseNetwork, self).__init__()
-
-        ########## YOUR CODE HERE ##########
-        # TODO: Here, create any layers and attributes your network needs.
         self.embedding = nn.Embedding(embeddings.shape[0], EMBEDDING_DIM)
         self.embedding.weight = nn.Parameter(embeddings.type(torch.FloatTensor), requires_grad = False)
         self.l1     = nn.Linear(EMBEDDING_DIM, 100)
@@ -20,14 +17,6 @@ class DenseNetwork(nn.Module):
         self.sm     = nn.Softmax()     
         
     def forward(self, x):
-        ########## YOUR CODE HERE ##########
-        # TODO: Fill in the forward pass of your neural network.
-        # TODO: (The backward pass will be performed by PyTorch magic for you!)
-        # TODO: Your architecture should...
-        # TODO: 1) Put the words through an Embedding layer (which was initialized with the pretrained embeddings);
-        # TODO: 2) Take the sum of all word embeddings in a sentence; and
-        # TODO: 3) Feed the result into 2-layer feedforward network which produces a 4-vector of values,
-        # TODO: one for each class
         x = self.embedding(x)
         x = torch.sum(x, dim = 1)
         x = self.l1(x)
@@ -50,13 +39,7 @@ class RecurrentNetwork(nn.Module):
 
     # x is a PaddedSequence for an RNN
     def forward(self, x):
-        ########## YOUR CODE HERE ##########
-        # TODO: (The backward pass will be performed by PyTorch magic for you!)
-        # TODO: Your architecture should...
-        # TODO: 1) Put the words through an Embedding layer (which was initialized with the pretrained embeddings);
-        # TODO: 2) Feed the sequence of embeddings through a 2-layer RNN; and
-        # TODO: 3) Feed the last output state into a dense layer to become a 4-vector of values, one for each class
-
+        
         x = self.embedding(x)
         lens = torch.zeros(x.shape[0])
         
@@ -76,18 +59,4 @@ class RecurrentNetwork(nn.Module):
 
         return x
         
-        
-# TODO: If you do any extensions that require you to change your models, make a copy and change it here instead.
-# TODO: PyTorch unfortunately requires us to have your original class definitions in order to load your saved
-# TODO: dense and recurrent models in order to grade their performance.
-class ExperimentalNetwork(nn.Module):
-    def __init__(self):
-        super(ExperimentalNetwork, self).__init__()
-
-        ########## YOUR CODE HERE ##########
-        raise NotImplementedError
-
-    # x is a PaddedSequence for an RNN
-    def forward(self, x):
-        ########## YOUR CODE HERE ##########
-        raise NotImplementedError
+   
